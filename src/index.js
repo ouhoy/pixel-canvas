0f408aee2d8c1ad8f820d117951981edbe758104
ouhoy <kimkinon@gmail.com>
2023-07-18 22:46:55 +0100

    Change color of the canvas pixel on click

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-var pixels = [];
+"use strict";
+const pixels = [];
 function createCanvas() {
-    var canvasSize = 8;
-    var canvas = document.getElementById("canvas");
-    for (var x = 0; x < canvasSize; x++) {
-        for (var y = 0; y < canvasSize; y++) {
-            pixels.push({ x: x, y: y, active: false });
-            var pixelBtn = createPixel({ x: x, y: y, active: false });
-            canvas.append(pixelBtn);
+    const canvasSize = 8;
+    const canvas = document.getElementById("canvas");
+    for (let x = 0; x < canvasSize; x++) {
+        for (let y = 0; y < canvasSize; y++) {
+            pixels.push({ x, y, active: false });
+            const pixelBtn = createPixel({ x, y, active: false });
+            canvas === null || canvas === void 0 ? void 0 : canvas.append(pixelBtn);
         }
     }
 }
 function createPixel(pixel) {
-    var pixelBtn = document.createElement('button');
+    const pixelBtn = document.createElement('button');
+    pixelBtn.id = `${pixel.x}-${pixel.y}`;
     pixelBtn.addEventListener("click", function () {
+        updateState(pixel.x, pixel.y);
+        refreshCanvas();
     });
     return pixelBtn;
+}
+function updateState(x, y) {
+    const pixelToUpdate = pixels.find(p => p.x == x && p.y == y);
+    if (pixelToUpdate)
+        pixelToUpdate.active = !pixelToUpdate.active;
+}
+function refreshCanvas() {
+    for (const p of pixels) {
+        const pixelBtn = document.getElementById(`${p.x}-${p.y}`);
+        if (pixelBtn)
+            pixelBtn.style.backgroundColor =
+                p.active ? '#28bb84' : '#efefe1';
+    }
 }
 createCanvas();
--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,35 @@ function createCanvas() {
             pixels.push({x, y, active: false})
 
             const pixelBtn = createPixel({x, y, active: false})
-            canvas.append(pixelBtn)
+            canvas?.append(pixelBtn)
         }
     }
 }
 
 function createPixel(pixel: { x: number, y: number, active: boolean }): HTMLButtonElement {
     const pixelBtn = document.createElement('button');
-    
-    pixelBtn.addEventListener("click", ()=>{
-
+    pixelBtn.id = `${pixel.x}-${pixel.y}`;
+    pixelBtn.addEventListener("click", function () {
+        updateState(pixel.x, pixel.y)
+        refreshCanvas()
     })
 
     return pixelBtn
+}
+
+function updateState(x: number, y: number): void {
+    const pixelToUpdate = pixels.find(p => p.x == x && p.y == y);
+    if (pixelToUpdate)
+        pixelToUpdate.active = !pixelToUpdate.active;
+}
+
+function refreshCanvas(): void {
+    for (const p of pixels) {
+        const pixelBtn = document.getElementById(`${p.x}-${p.y}`);
+        if (pixelBtn)
+            pixelBtn.style.backgroundColor =
+                p.active ? '#28bb84' : '#efefe1';
+    }
 }
 
 createCanvas()
