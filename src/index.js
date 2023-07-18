707d6a53f1293990de3d0afbabc75706e692b599
ouhoy <kimkinon@gmail.com>
2023-07-18 22:32:16 +0100

    Render Pixels

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("ts");
+var pixels = [];
+function createCanvas() {
+    var canvasSize = 8;
+    var canvas = document.getElementById("canvas");
+    for (var x = 0; x < canvasSize; x++) {
+        for (var y = 0; y < canvasSize; y++) {
+            pixels.push({ x: x, y: y, active: false });
+            var pixelBtn = createPixel({ x: x, y: y, active: false });
+            canvas.append(pixelBtn);
+        }
+    }
+}
+function createPixel(pixel) {
+    var pixelBtn = document.createElement('button');
+    pixelBtn.addEventListener("click", function () {
+    });
+    return pixelBtn;
+}
+createCanvas();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("ts")
+const pixels: { x: number, y: number, active: boolean }[] = []
+
+function createCanvas() {
+
+    const canvasSize = 8;
+    const canvas = document.getElementById("canvas")
+
+    for (let x = 0; x < canvasSize; x++) {
+        for (let y = 0; y < canvasSize; y++) {
+            pixels.push({x, y, active: false})
+
+            const pixelBtn = createPixel({x, y, active: false})
+            canvas.append(pixelBtn)
+        }
+    }
+}
+
+function createPixel(pixel: { x: number, y: number, active: boolean }): HTMLButtonElement {
+    const pixelBtn = document.createElement('button');
+    
+    pixelBtn.addEventListener("click", ()=>{
+
+    })
+
+    return pixelBtn
+}
+
+createCanvas()
